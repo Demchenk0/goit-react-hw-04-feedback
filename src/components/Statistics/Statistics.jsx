31d91015b120e36a0fe37d0661fc5a84e9e7cf03
Demchenk0 <santadyr2016@gmail.com>
2022-09-20 00:28:08 +0300

    proptypes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -21,8 +21,8 @@ export const Statistics = ({
 
 Statistics.propTypes = {
     good:PropTypes.number.isRequired,
-	neutral:PropTypes.string.isRequired,
-	bad:PropTypes.string.isRequired,
-	total:PropTypes.string.isRequired,
-	positivePercentage:PropTypes.string.isRequired,
+	neutral:PropTypes.number.isRequired,
+	bad:PropTypes.number.isRequired,
+	total:PropTypes.number.isRequired,
+	positivePercentage:PropTypes.number.isRequired,
 }
