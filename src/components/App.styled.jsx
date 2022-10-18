4a69fed0dca002e216a39f30b94389a10ab3c654
Demchenk0 <santadyr2016@gmail.com>
2022-10-18 23:50:14 +0300

    sad

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled';
 
 export const GeneralContainer = styled.div`
-	@media screen and (min-width: 320px) {
-		width: 500px;
-		display: flex;
-		text-align: center;
-		justify-content: center;
-		flex-direction: column;
-		margin: auto;
-	}
+	/* width: 500px; */
+	display: flex;
+	text-align: center;
+	justify-content: center;
+	flex-direction: column;
+	margin: auto;
 `;
+// @media screen and(min - width: 320px) { }
