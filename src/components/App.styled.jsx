a94bff9bc37f104398f69e71d6d907b32dffa74f
Demchenk0 <santadyr2016@gmail.com>
2022-10-18 23:41:36 +0300

    add

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
 export const GeneralContainer = styled.div`
-	width: 500px;
-	display: flex;
-	text-align: center;
-	justify-content: center;
-	flex-direction: column;
-	margin: auto;
+	@media screen and (min-width: 320px) {
+		width: 500px;
+		display: flex;
+		text-align: center;
+		justify-content: center;
+		flex-direction: column;
+		margin: auto;
+	}
 `;
